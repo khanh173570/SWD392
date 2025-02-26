8c7ec672d9d0e6885e6d4d006be76a466b11ec1d
PHUONG_KHANH\tranp <khanhtpse173570@fpt.edu.vn>
2025-02-27 05:03:51 +0700

    update layout 27/02

--- src/components/componentCustomer/componentsHeader/componentsHeader.jsx
+++ src/components/componentCustomer/componentsHeader/componentsHeader.jsx
@@ -2,11 +2,12 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
 import { NavLink, useNavigate } from "react-router-dom";
-import { Row, Col, Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import "./ComponentsHeader.css";
 import useAuth from "../../../utils/hook/useAuth";
 import { logout } from "../../../contexts/AuthContext/reducer";
-import { FaSignInAlt } from "react-icons/fa";
+import { FaUser } from "react-icons/fa"; // Đổi sang icon người dùng
+import { FaBars } from "react-icons/fa";
 import PRE from "../../../assets/asstetsCustomer/react.svg";
 
 function ComponentsHeader() {
@@ -20,222 +21,112 @@ function ComponentsHeader() {
   };
 
   return (
-    <Navbar
-      expand="md"
-      className="nav-header navbar navbar-expand-md navbar-light w-100"
-    >
-      <Row className=" align-items-center w-100">
-        <Col
-          md={5}
-          style={{ height: "100px" }}
-          className=" text-black  p-4 text-center"
-        >
-          <Navbar.Brand href="/" className="p-3 fw-bold">
-            <img
-              src={PRE}
-              width="50px"
-              height="50px"
-              alt="Logo"
-              style={{ marginRight: "50px" }}
-            />
-            PRE MARITIAL 2025
-          </Navbar.Brand>
-        </Col>
-        <Col
-          md={7}
-          style={{ height: "100px" }}
-          className=" text-black p-3 text-center ms-auto"
-        >
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <div className="p-2 fw-bold ">
-              <Nav>
-                <NavLink to="/" className="nav-link">
-                  Home
-                </NavLink>
+    <Navbar expand="md" className="nav-header navbar-light w-100">
+      <Container>
+        <Navbar.Brand href="/" className="fw-bold d-flex align-items-center">
+          <img
+            src={PRE}
+            width="50px"
+            height="50px"
+            alt="Logo"
+            className="me-3"
+          />
+          PRE MARITIAL 2025
+        </Navbar.Brand>
 
-                <NavDropdown title="Getting Married" id="nav-dropdown">
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product01"
-                    className="nav-dropdown-item"
-                  >
-                    Pre Marriage Counselling
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product02"
-                    className="nav-dropdown-item"
-                  >
-                    Marriage Readiness
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product03"
-                    className="nav-dropdown-item"
-                  >
-                    Marriage Vows
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product04"
-                    className="nav-dropdown-item"
-                  >
-                    Marriage Preparation
-                  </NavDropdown.Item>
-                </NavDropdown>
+        {/* Nút mở menu trên mobile */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <FaBars size={24} />
+        </Navbar.Toggle>
 
-                <NavDropdown title="Married Life" id="nav-dropdown">
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product01"
-                    className="nav-dropdown-item"
-                  >
-                    Relationship
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product02"
-                    className="nav-dropdown-item"
-                  >
-                    Love
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product03"
-                    className="nav-dropdown-item"
-                  >
-                    Communication
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product04"
-                    className="nav-dropdown-item"
-                  >
-                    Intimacy
-                  </NavDropdown.Item>
-                </NavDropdown>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {/* <NavLink to="/" className="nav-link">
+              Home
+            </NavLink> */}
 
-                <NavDropdown title="Married Life" id="nav-dropdown">
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product01"
-                    className="nav-dropdown-item"
-                  >
-                    Relationship
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product02"
-                    className="nav-dropdown-item"
-                  >
-                    Love
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product03"
-                    className="nav-dropdown-item"
-                  >
-                    Communication
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product04"
-                    className="nav-dropdown-item"
-                  >
-                    Intimacy
-                  </NavDropdown.Item>
-                </NavDropdown>
+            <NavDropdown title="Getting Married" id="nav-dropdown">
+              <NavDropdown.Item as={NavLink} to="/product01">
+                Pre Marriage Counselling
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product02">
+                Marriage Readiness
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product03">
+                Marriage Vows
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product04">
+                Marriage Preparation
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Getting Married" id="nav-dropdown">
+              <NavDropdown.Item as={NavLink} to="/product01">
+                Pre Marriage Counselling
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product02">
+                Marriage Readiness
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product03">
+                Marriage Vows
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product04">
+                Marriage Preparation
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Getting Married" id="nav-dropdown">
+              <NavDropdown.Item as={NavLink} to="/product01">
+                Pre Marriage Counselling
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product02">
+                Marriage Readiness
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product03">
+                Marriage Vows
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product04">
+                Marriage Preparation
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Getting Married" id="nav-dropdown">
+              <NavDropdown.Item as={NavLink} to="/product01">
+                Pre Marriage Counselling
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product02">
+                Marriage Readiness
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product03">
+                Marriage Vows
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/product04">
+                Marriage Preparation
+              </NavDropdown.Item>
+            </NavDropdown>
 
-                <NavDropdown title="Married Life" id="nav-dropdown">
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product01"
-                    className="nav-dropdown-item"
-                  >
-                    Relationship
+            {/* Dropdown User */}
+            <NavDropdown
+              title={<FaUser size={20} />}
+              id="user-dropdown"
+              align="end"
+            >
+              {user ? (
+                <>
+                  <NavDropdown.Item as={NavLink} to="/profile">
+                    My Profile
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product02"
-                    className="nav-dropdown-item"
-                  >
-                    Love
+                  <NavDropdown.Item onClick={handleLogout}>
+                    Log out
                   </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product03"
-                    className="nav-dropdown-item"
-                  >
-                    Communication
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to="/product04"
-                    className="nav-dropdown-item"
-                  >
-                    Intimacy
-                  </NavDropdown.Item>
-                </NavDropdown>
-                {user ? (
-                  <NavDropdown
-                    title={`${user.firstName} ${user.lastName}`}
-                    id="nav-dropdown"
-                  >
-                    <NavDropdown.Item
-                      as={NavLink}
-                      to="/profile"
-                      className="nav-dropdown-item"
-                    >
-                      My Profile
-                    </NavDropdown.Item>
-
-                    <NavDropdown.Item className="nav-dropdown-item">
-                      <Button
-                        onClick={handleLogout}
-                        className="btn btn-danger w-100 text-start"
-                        style={{
-                          border: "none",
-                          background: "none",
-                          padding: "5px 15px",
-                        }}
-                      >
-                        Log out
-                      </Button>
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                ) : (
-                  <div style={{ marginBottom: "10px" }}>
-                    <button
-                      onClick={() => navigate("/login")}
-                      className="login-button"
-                      style={{
-                        borderRadius: "10px",
-                      }}
-                    >
-                      <FaSignInAlt style={{ marginRight: "8px" }} />
-                      LOGIN
-                    </button>
-                  </div>
-                )}
-              </Nav>
-            </div>
-          </Navbar.Collapse>
-        </Col>
-      </Row>
-
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                </>
+              ) : (
+                <NavDropdown.Item onClick={() => navigate("/login")}>
+                  Login
+                </NavDropdown.Item>
+              )}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
